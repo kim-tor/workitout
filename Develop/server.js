119d02e08b56a994914557d7032322c3782ae1ef
Kim Torres <kimberley.5118@gmail.com>
2021-01-07 10:38:22 -0500

    created html route file

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
@@ -17,7 +20,7 @@ mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/workout",
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/apiRoutes.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
