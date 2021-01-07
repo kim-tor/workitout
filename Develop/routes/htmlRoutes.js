02ebe71a14f9781e2ae891728b2096a93a757278
Kim Torres <kimberley.5118@gmail.com>
2021-01-07 11:01:24 -0500

    error in get functions html routes

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,16 +1,16 @@
 const router = require("express").Router();
 const path = require("path");
 
-router.get("/", (req, res) => {
-    res.sendFile(path.join(_direname + "../public/index.html"));
-});
+router.get("/", (req, res => {
+    res.sendFile(path.join(_dirname + "../public/index.html"));
+}));
 
-router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(_direname + "../public/exercise.html"));
-});
+router.get("/exercise", (req, res => {
+    res.sendFile(path.join(_dirname + "../public/exercise.html"));
+}));
 
 router.get("/stats", (req, res => {
-    res.sendFIle(path.join(_direname + "../public/stats.html"));
+    res.sendFIle(path.join(_dirname + "../public/stats.html"));
 }));
 
 module.exports = router;
