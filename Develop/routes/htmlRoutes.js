532069a6ccc7f7708f50f66b80c1b405f8e32d0f
Kim Torres <kimberley.5118@gmail.com>
2021-01-07 10:49:07 -0500

    errors in spelling and module export in htmlroutes

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,15 +1,16 @@
 const router = require("express").Router();
 const path = require("path");
 
-router.get("/", (req,res) => {
-    res.sendFile(path.join(_direname + "/public/index.html"));
+router.get("/", (req, res) => {
+    res.sendFile(path.join(_direname + "../public/index.html"));
 });
 
-router.get("/exercise", (req, rest) => {
-    res.sendFile(path.join(_direname + "/public/exercise.html"));
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(_direname + "../public/exercise.html"));
 });
 
 router.get("/stats", (req, res => {
-    res.sendFIle(path.join(_direname + "/public/stats.html"));
+    res.sendFIle(path.join(_direname + "../public/stats.html"));
 }));
 
+module.exports = router;
