659e9f9dd6925dc956a3541fbeccd2640ec6d3ff
Kim Torres <kimberley.5118@gmail.com>
2021-01-07 11:38:38 -0500

    fixed my dirname in all my html routes

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/workout",
 });
 
 // routes
-app.use(require("routes/apiRoutes.js"));
-app.use(require("routes/htmlRoutes.js"));
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
